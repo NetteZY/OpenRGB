afa362c9e120ccbfda5de913a898079bf5b46613
kuraaku <nsasuke355@gmail.com>
2026-07-12 15:42:25 +0800

    fix: use DetectionManager.h and DetectedControllers API

--- Controllers/RakkLamAngV1Controller/RakkLamAngV1ControllerDetect.cpp
+++ Controllers/RakkLamAngV1Controller/RakkLamAngV1ControllerDetect.cpp
@@ -1,18 +1,21 @@
-#include "Detector.h"
+#include <hidapi.h>
+#include "DetectionManager.h"
 #include "RakkLamAngV1Controller.h"
 #include "RGBController_RakkLamAngV1.h"
-#include "ResourceManager.h"
 
-void DetectRakkLamAngV1Keyboards(hid_device_info* info, const std::string& /*name*/)
+DetectedControllers DetectRakkLamAngV1Keyboards(hid_device_info* info, const std::string& /*name*/)
 {
+    DetectedControllers detected_controllers;
     hid_device* dev = hid_open_path(info->path);
 
     if (dev)
     {
         RakkLamAngV1Controller* controller = new RakkLamAngV1Controller(dev, info->path);
         RGBController_RakkLamAngV1* rgb_controller = new RGBController_RakkLamAngV1(controller);
-        ResourceManager::get()->RegisterRGBController(rgb_controller);
+        detected_controllers.push_back(rgb_controller);
     }
+
+    return(detected_controllers);
 }
 
 REGISTER_HID_DETECTOR_I("RAKK Lam-Ang Lite V1", DetectRakkLamAngV1Keyboards, 0x0C45, 0x8006, 0);
